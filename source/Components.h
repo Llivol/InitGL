aa4e06a9c4397bebe706691ed0c644be25ad4937
llivol <adriapuigdellivol@gmail.com>
2019-10-29 08:41:22 +0100

    29/10 ECS class

--- source/Components.h
+++ source/Components.h
@@ -3,20 +3,67 @@
 #include "includes.h"
 #include "Texture.h"
 #include <vector>
+#include <tuple>
 
 /**** COMPONENTS ****/
 
 //Component (base class)
 // - owner: id of Entity which owns the instance of the component
-struct Component {
+struct Component
+{
     int owner;
 };
 
-
-/**** ENTITY ****/
-
-struct Entity {
-
-
+struct Transform : public Component, public lm::mat4
+{
+    //     int parent = -1;
+    //     lm::mat4 getGlobalMatrix(std::vector<Transform> &transforms)
+    //     {
+    //         if (parent != -1)
+    //         { // if I have a model matrix parent
+    //             // multiply this model by all of its parents
+    //             // to access in a vector:
+    //             // transforms[i]
+    //             // transforms.at(i)
+    //             return transforms.at(parent).getGlobalMatrix(transforms) * *this;
+    //         }
+    //         else
+    //         {
+    //             return *this; // this returns the copy of the memory, NOT the address
+    //         }
+    //     }
 };
 
+struct Mesh : public Component
+{
+    // TODO fill in mesh data
+};
+
+typedef std::tuple<
+    std::vector<Transform>,
+    std::vector<Mesh>>
+    ComponentArrays;
+
+const int NUM_TYPE_COMPONENTS = 2;
+/**** ENTITY ****/
+
+struct Entity
+{
+    std::string name;
+    bool active = true;
+    int components[NUM_TYPE_COMPONENTS];
+    Entity()
+    {
+        for (int i = 0; i < NUM_TYPE_COMPONENTS; i++)
+        {
+            components[i] = -1;
+        }
+    }
+    Entity(std::string a_name) : name(a_name)
+    {
+        for (int i = 0; i < NUM_TYPE_COMPONENTS; i++)
+        {
+            components[i] = -1;
+        }
+    }
+};
--- source/EntityComponentStore.h
+++ source/EntityComponentStore.h
@@ -12,7 +12,9 @@ using namespace std;
 //all the entities, and an array to store each of the component types
 struct EntityComponentStore {
     
-    
+    vector<Entity> entities;
+
+    ComponentArrays components; // tuple of arrays
     
     //create Entity and add transform component by default
     //return array id of new entity
@@ -20,11 +22,18 @@ struct EntityComponentStore {
 //
 //    }
 //
-//    //creates a new component with no entity parent
-//    template<typename T>
-//    int createComponent(){
-//
-//    }
+   //creates a new component with no entity parent
+   template<typename T>
+   int createComponent(){
+       vector<T>& the_comps = get<vector<T>>( components );
+
+        Transform my_transform;
+
+        // creates a new object directy on the vector
+        the_comps.emplace_back();
+
+       return (int)the_comps.size() - 1; // index of new transform in array
+   }
 //
 //    //creates a new component and associates it with an entity
 //    template<typename T>
